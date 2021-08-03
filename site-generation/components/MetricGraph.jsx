be1d502e80d9d9a30a3a0a13939aee221b4aa7f0
Evan Williams <evanwi@zillowgroup.com>
2021-08-03 08:16:51 -0700

    chore(site): Better height limitations on the graph

--- site-generation/components/MetricGraph.jsx
+++ site-generation/components/MetricGraph.jsx
@@ -1,7 +1,5 @@
 import { VictoryChart, VictoryBoxPlot } from 'victory';
-import {
-  Box, Flex, ListItem, Text, UnorderedList,
-} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import useResults from '../hooks/results';
@@ -11,38 +9,23 @@ const MetricGraph = ({ metric, ...props }) => {
   const {
     loaded, failed, data, newURL, comparisonURL,
   } = useResults();
-  const { legends, data: plotData } = useMemo(() => (loaded && !failed ? Object.entries(data)
-    .reduce(({ legends: accLegends, data: accData }, [path, { new: newSite, comparison }], i) => {
-      const newLabel = String.fromCharCode('A'.charCodeAt(0) + (i * 2));
-      const comparisonLabel = String.fromCharCode('A'.charCodeAt(0) + (i * 2 + 1));
-      return ({
-        legends: {
-          ...accLegends,
-          [newLabel]: `${newURL}${path}`,
-          [comparisonLabel]: `${comparisonURL}${path}`,
-        },
-        data: [
-          ...accData,
-          { x: newLabel, y: newSite.map((obj) => obj[metric]) },
-          { x: comparisonLabel, y: comparison.map((obj) => obj[metric]) },
-        ],
-      });
-    }, { legends: [], data: [] }) : []), [data, newURL, comparisonURL, metric, loaded, failed]);
+  const plotData = useMemo(() => (loaded && !failed ? Object.entries(data)
+    .reduce((acc, [path, { new: newSite, comparison }]) => ([
+      ...acc,
+      { x: `${newURL}${path}`, y: newSite.map((obj) => obj[metric]) },
+      { x: `${comparisonURL}${path}`, y: comparison.map((obj) => obj[metric]) },
+    ]), []) : []), [data, newURL, comparisonURL, metric, loaded, failed]);
+  const chartHeight = useMemo(() => 100 * plotData.length, [plotData.length]);
   return loaded && !failed && (
-  <Box {...props}>
-    <UnorderedList appearance="">
-      {Object.entries(legends).map(([series, label]) => (
-        <ListItem key={series}><Flex><Text as="strong" mr={1}>{series}:</Text><Text>{label}</Text></Flex></ListItem>
-      ))}
-    </UnorderedList>
-    <VictoryChart domainPadding={20} height={75 * plotData.length}>
-      <VictoryBoxPlot
-        horizontal
-        boxWidth={20}
-        data={plotData}
-      />
-    </VictoryChart>
-  </Box>
+  <Box {...props} height={chartHeight} pb={4}>
+      <VictoryChart domainPadding={20} height={chartHeight}>
+        <VictoryBoxPlot
+          horizontal
+          boxWidth={20}
+          data={plotData}
+        />
+      </VictoryChart>
+    </Box>
   );
 };
 MetricGraph.propTypes = {
